acb722220eb1b2d9250f4b660e6039f2881e14fb
sahib-ldh <itiglehunt@gmail.com>
2025-03-20 21:41:59 +0530

    parsing value through function

---
+++ 14.0 JavaScript/1.1_parsingValueinFunction.js
@@ -0,0 +1,25 @@
+function getMilk(money) { 
+    var numberOfPacket = money/33;
+    console.log("leaveHouse");
+    console.log("moveRight");
+    console.log("moveRight");
+    console.log("moveUp");
+    console.log("moveUp");
+    console.log("moveUp");
+    console.log("moveUp");
+    console.log("moveRight");
+    console.log("moveRight");
+    console.log("get " + parseInt(numberOfPacket) + " packets of milk");
+    console.log("moveLeft");
+    console.log("moveLeft");
+    console.log("moveDown");
+    console.log("moveDown");
+    console.log("moveDown");
+    console.log("moveDown");
+    console.log("moveLeft");
+    console.log("moveLeft");
+    console.log("enterHouse");
+    console.log("The milk packet we got with money : " + parseInt(numberOfPacket)  );
+  
+  }
+  getMilk(472);
