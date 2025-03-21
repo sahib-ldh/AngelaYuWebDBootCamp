af8c3f248598208dd1d164e83f86c9a763dee7ac
sahib-ldh <itiglehunt@gmail.com>
2025-03-21 11:33:11 +0530

    JS functions

--- 14.0 JavaScript/1.1_parsingValueinFunction.js
+++ 14.0 JavaScript/1.1_parsingValueinFunction.js
@@ -1,25 +1,40 @@
-function getMilk(money) { 
-    var numberOfPacket = money/33;
-    console.log("leaveHouse");
-    console.log("moveRight");
-    console.log("moveRight");
-    console.log("moveUp");
-    console.log("moveUp");
-    console.log("moveUp");
-    console.log("moveUp");
-    console.log("moveRight");
-    console.log("moveRight");
-    console.log("get " + parseInt(numberOfPacket) + " packets of milk");
-    console.log("moveLeft");
-    console.log("moveLeft");
-    console.log("moveDown");
-    console.log("moveDown");
-    console.log("moveDown");
-    console.log("moveDown");
-    console.log("moveLeft");
-    console.log("moveLeft");
-    console.log("enterHouse");
-    console.log("The milk packet we got with money : " + parseInt(numberOfPacket)  );
+function getMilk(money,costPerPacket) { 
   
-  }
-  getMilk(472);
+  console.log("leaveHouse");
+  console.log("moveRight");
+  console.log("moveRight");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveRight");
+  console.log("moveRight");
+  console.log("get " + calPacket(money,costPerPacket) + " packets of milk");
+  console.log("moveLeft");
+  console.log("moveLeft");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveLeft");
+  console.log("moveLeft");
+  console.log("enterHouse");
+  console.log("The milk packet we got with money : " + calPacket(money,costPerPacket)  );
+  return calcChange(money,costPerPacket);
+
+}
+function calPacket(StartingMoney,costPerPacket)
+{
+  var numberOfPacket = Math.floor(StartingMoney/costPerPacket)
+  return numberOfPacket;
+}
+
+function calcChange(StartingMoney, costPerPacket)
+{
+  var change = StartingMoney % costPerPacket;
+  return change;
+}
+
+console.log("hey sahib, here is your " + getMilk(100,33) + " change sahibb");
+
+
