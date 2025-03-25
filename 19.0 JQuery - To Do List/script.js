39d9c20c45579d6429c8a5ff488e51f39ff1fe0f
sahib-ldh <itiglehunt@gmail.com>
2025-03-25 15:51:22 +0530

    JQuery To Do List

---
+++ 19.0 JQuery - To Do List/script.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+    // Toggle completed class on click
+    $('#todo-list').on('click', 'li', function() {
+      $(this).toggleClass('completed');
+    });
+  
+    // Add new task
+    $('#add-task').click(function() {
+      const taskText = $('#new-task').val();
+      if (taskText.trim() !== '') {
+        $('#todo-list').append('<li>' + taskText + '</li>');
+        $('#new-task').val(''); // clear input
+      } else {
+        alert("Please enter a task.");
+      }
+    });
+  
+    // Add enter key support
+    $('#new-task').keypress(function(e) {
+      if (e.which === 13) {
+        $('#add-task').click();
+      }
+    });
+  });
+  
